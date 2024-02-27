b6a76b0a7caba0e6d2197347861df0efc5656d56
John <johntaykimani@gmail.com>
2024-02-27 11:53:04 +0300

    Feat: Function confirms the number is not a float and positive.

--- binaryGap.js
+++ binaryGap.js
@@ -1,1 +1,7 @@
 // Binary Gap
+
+function solution(N) {
+    if (N > 0 && Number.isInteger(N) === true){
+ return N.toString(2)
+} return "Enter A Positive Number That Is Not A Float. INTEGERS ONLY"
+}
