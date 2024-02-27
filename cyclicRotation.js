dfeb67ede6f15f2831ec619d19424cd7b4901eb6
John <johntaykimani@gmail.com>
2024-02-27 22:36:12 +0300

    Feat: Added Validation of parameters

--- cyclicRotation.js
+++ cyclicRotation.js
@@ -2,5 +2,9 @@
 // Rotate an array to the right by a given number of steps
 
 function solution(A, K) {
+// Validate that A is an array and K is an integer
+    if (Array.isArray(A)=== true && Number.isInteger(K) === true && K > 0 ){
     
+// If the validation is falsy we get this output
+    } else return "Param A has to be a valid Array and Param K has to be a POSITIVE integer" 
 }
