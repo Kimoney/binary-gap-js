908f957766a688160328f43e997e1ac7630119b1
John <johntaykimani@gmail.com>
2024-02-27 12:30:26 +0300

    Feat: Count The Zeros Sussessfully

--- binaryGap.js
+++ binaryGap.js
@@ -2,7 +2,25 @@
 
 function solution(N) {
     if (N > 0 && Number.isInteger(N) === true){
-        let binary = N.toString(2)
- return binary
-} return "Enter A Positive Number That Is Not A Float. INTEGERS ONLY"
+    // Convert Number To Binary
+        const binary = N.toString(2);
+    // define maximum gap, current gap, and counting state
+        let maxGap = 0;
+        let currentGap = 0;
+        let countingState = false;
+    // Loop over the binary number to act on it accordingly
+    for ( let i=0; i < binary.length; i++){
+        if(binary[i] === "1"){
+            if (currentGap > maxGap){
+                maxGap = currentGap;
+            }
+            currentGap = 0;
+            countingState = true;
+        } else if (countingState){
+            currentGap++
+        }
+    }
+    // Return the value  of Maximum Gap using the outermost conditionnal
+    return maxGap
+    } return "Enter A Positive Number That Is Not A Float. INTEGERS ONLY"
 }
